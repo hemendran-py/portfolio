8ce2e186c5df4a6737a08d8851edc7eed7c34bdc
hemendran-py <hemendrankumar@gmail.com>
2025-02-10 17:00:00 +0530

    time

--- components/TimePicker.js
+++ components/TimePicker.js
@@ -1,31 +1,22 @@
-"use client"
-import Location from "@/components/location";
+"use client";
 import React, { useEffect, useState } from "react";
 
-
-
-
 function TimePicker() {
-    const [time, setTime] = useState(new Date());
+    const [time, setTime] = useState(null); // Start with null to avoid mismatch
 
     useEffect(() => {
+        setTime(new Date()); // Set correct time immediately on mount
         const intervalId = setInterval(() => setTime(new Date()), 1000);
         return () => clearInterval(intervalId); // Clear interval on component unmount
     }, []);
 
+    if (!time) return null; // Prevent rendering until the correct time is set
+
     return (
         <div>
-            <div >
-                
-
-                <p className=" text-white timepicker-container    " style={{ letterSpacing: '1px' }}>
-                
-
-                    
-                    INDIA / {time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).toUpperCase()}
-                    
-                </p>
-            </div>
+            <p className="text-white timepicker-container" style={{ letterSpacing: '1px' }}>
+                INDIA / {time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).toUpperCase()}
+            </p>
         </div>
     );
 }
